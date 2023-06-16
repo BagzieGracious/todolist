59975d56516ecc4cb833146302f784a97e0311a2
BagzieGracious <bagendadeogracious@gmail.com>
2023-06-16 15:26:32 +0300

    added components and needed logic

--- src/App.js
+++ src/App.js
@@ -1,56 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import {
+  useGetTodosQuery,
+  useCreateTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} from "./api/todos";
+import { formatMonth } from "./mixins/dateUtils";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [item, setItem] = useState({ id: 0, name: "", date: "", time: "" });
+  const [disabled, setDisabled] = useState(true);
+  const { data: todos = [], refetch: refetchTodos } = useGetTodosQuery();
+  const [createTodo] = useCreateTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+
+  const handleAddTodo = async (e) => {
+    e.preventDefault();
+    if (!item.id) {
+      await createTodo({ ...item });
+    } else {
+      await updateTodo({ ...item });
+    }
+    refetchTodos();
+    setItem({ name: "", date: "", time: "" });
+    setDisabled(true)
+  };
+
+  const handleDeleteTodo = async (id) => {
+    await deleteTodo(id);
+    refetchTodos();
+  };
+
+  const handleEditTodo = (item) => {
+    setItem({ id: item.id, name: item.name, date: item.date, time: item.time });
+  };
+
+  const onChangeHandler = (e) => {
+    const currentDate = new Date();
+    const date = `${currentDate.getDate()}/${formatMonth(
+      currentDate.getMonth()
+    )}/${currentDate.getFullYear()}`;
+    const time = currentDate.toLocaleTimeString("en-US", {
+      hour12: false,
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+    const updatedItem = {
+      ...item,
+      name: e.target.value,
+      date,
+      time,
+    };
+    setItem(updatedItem);
+    setDisabled(!e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="container">
+      <h1>TODO List</h1>
+
+      <TodoForm
+        handleAddTodo={handleAddTodo}
+        onChangeHandler={onChangeHandler}
+        item={item}
+        disabled={disabled}
+      />
+
+      <TodoList
+        todos={todos}
+        onDeleteTodo={handleDeleteTodo}
+        onEditTodo={handleEditTodo}
+      />
     </div>
   );
 }
